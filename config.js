d48a1f61fff9dbb7660ed390089af1c28240d7bd
Filipp Shumakov <shum8929@gmail.com>
2020-10-19 19:47:58 +0300

    hofix:change dev db to camelCase

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   PORT: 3000,
-  MONGO_ADRESS: (process.env.NODE_ENV === 'production') ? process.env.MONGO_ADRESS : 'mongodb://localhost:27017/newsdb_dev',
+  MONGO_ADRESS: (process.env.NODE_ENV === 'production') ? process.env.MONGO_ADRESS : 'mongodb://localhost:27017/newsdbDev',
   JWT_SECRET: (process.env.NODE_ENV === 'production') ? process.env.JWT_SECRET : 'dev-secret',
 };
