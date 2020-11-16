a04b0d54609436c2a092193af0e5d67127869adf
Filipp Shumakov <shum8929@gmail.com>
2020-11-16 22:49:11 +0300

    hotfix:cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
