055619d3c7fa429331c721ce987879f3fd79282d
Filipp Shumakov <shum8929@gmail.com>
2020-11-16 22:59:05 +0300

    hotfix: cors.

--- controllers/users.js
+++ controllers/users.js
@@ -69,7 +69,7 @@ const login = (req, res, next) => {
     res.cookie('jwt', token, {
       maxAge: 3600000 * 24 * 7,
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: true,
     }).send({ message: successMessage.AUTH_SUCCESSFUL });
   }).catch(next);
 };
