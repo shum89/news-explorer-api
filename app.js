88f46a960561ae64916d8bb4fb0d057200644b97
Filipp Shumakov <shum8929@gmail.com>
2020-11-17 21:22:20 +0300

    hotfix: cors.

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ const { limiter } = require('./utils/limiter');
 const { router } = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
+const corsOptions = {
+  credentials: true,
+  origin: ['http://localhost:3000', 'https://localhost:3000'],
+};
 const app = express();
 app.use(helmet());
-app.use(cors());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(limiter);
